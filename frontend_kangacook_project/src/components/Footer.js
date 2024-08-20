20738d5dd0c7656905c88ddfde47777ab6c3adc7
bacdor <bachlaj.dorian@gmail.com>
2024-08-20 11:24:46 -0400

    basic frontend setup

---
+++ frontend_kangacook_project/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import { useState, useEffect } from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend_kangacook_project/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+
+function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
+
+export default Header;
