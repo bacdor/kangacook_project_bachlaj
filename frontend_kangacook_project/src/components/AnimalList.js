6e6699ca11b97d8966eed918a70c9ff5ee9b24f8
bacdor <bachlaj.dorian@gmail.com>
2024-08-21 09:54:52 -0400

    animals list

--- frontend_kangacook_project/src/components/AnimalList.js
+++ frontend_kangacook_project/src/components/AnimalList.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 const AnimalList = () => {
   const [animals, setAnimals] = useState([]);
+  //To provide a message
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
   useEffect(() => {
     const fetchAnimals = async () => {
@@ -10,7 +13,8 @@ const AnimalList = () => {
         const response = await axios.get("http://127.0.0.1:8000/app/");
         setAnimals(response.data);
       } catch (error) {
-        console.error("There was an error fetching the animals!", error);
+        setError("There was an error fetching the animals!", error);
+        setSuccess(null);
       }
     };
 
@@ -19,24 +23,48 @@ const AnimalList = () => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://127.0.0.1:8000/animals/${id}/`);
+      await axios.delete(`http://127.0.0.1:8000/app/${id}/`);
       setAnimals(animals.filter((animal) => animal.id !== id));
+      setSuccess("The animal deleted successfully!");
+      setError(null);
     } catch (error) {
-      console.error("There was an error deleting the animal!", error);
+      setError("Failed to delete the animal");
+      setSuccess(null);
     }
   };
 
   return (
     <div>
-      <h2>Animal List</h2>
-      <ul>
-        {animals.map((animal) => (
-          <li key={animal.id}>
-            <strong>{animal.name}</strong>: {animal.description}
-            <button onClick={() => handleDelete(animal.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+        Animal List
+      </h2>
+      <div class="overflow-x-auto">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Color</th>
+              <th>Legs</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {animals.map((item, index) => (
+              <tr key={item.id} className={index % 2 === 0 ? "" : "hover"}>
+                <td>{item.name}</td>
+                <td>{item.color}</td>
+                <td>{item.legs_counts}</td>
+                <td>{item.description}</td>
+                <td>
+                  <button onClick={() => handleDelete(item.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          {error && <div className="text-error">{error}</div>}
+          {success && <div className="text-success">{success}</div>}
+        </table>
+      </div>
     </div>
   );
 };
